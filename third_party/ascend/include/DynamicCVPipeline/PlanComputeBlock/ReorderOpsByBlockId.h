60e83eb2f9ec1f1422007a460b80e8b5f0a036f2
lowdy1 <xiahouweidong@gmail.com>
2026-06-26 08:17:37 +0000

    merge pipeline headers

--- third_party/ascend/include/DynamicCVPipeline/PlanComputeBlock/ReorderOpsByBlockId.h
+++ third_party/ascend/include/DynamicCVPipeline/PlanComputeBlock/ReorderOpsByBlockId.h
@@ -28,7 +28,6 @@
 
 namespace mlir::triton {
 
-<<<<<<< HEAD
 class ReorderOpsByBlockIdPass
     : public PassWrapper<ReorderOpsByBlockIdPass, OperationPass<ModuleOp>> {
 public:
@@ -36,18 +35,10 @@ public:
 
   ReorderOpsByBlockIdPass() = default;
   void runOnOperation() override;
-=======
-class ReorderOpsByBlockIdPass : public PassWrapper<ReorderOpsByBlockIdPass, OperationPass<ModuleOp>> {
-public:
-    MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(ReorderOpsByBlockIdPass);
 
-    ReorderOpsByBlockIdPass() = default;
-    void runOnOperation() override;
-
-    llvm::StringRef getArgument() const final {
+  llvm::StringRef getArgument() const final {
       return "reorder-ops-by-block-id";
-    }
->>>>>>> release-3.2.2-0625-b79d137
+  }
 };
 
 std::unique_ptr<OperationPass<ModuleOp>> createReorderOpsByBlockIdPass();
